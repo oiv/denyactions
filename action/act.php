c8d67cd9b8a5fe161b9b72ab680b96dd49e579f9
Otto Vainio <otto@valjakko.net>
2017-03-31 21:03:32 +0200

    Correct Register

--- action/act.php
+++ action/act.php
@@ -17,7 +17,7 @@ require_once DOKU_PLUGIN.'action.php';
 
 class action_plugin_denyactions_act extends DokuWiki_Action_Plugin {
 
-    public function register(Doku_Event_Handler &$controller) {
+    public function register(Doku_Event_Handler $controller) {
 
        $controller->register_hook('ACTION_ACT_PREPROCESS', 'BEFORE', $this, 'handle_action_act_preprocess');
    
